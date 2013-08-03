035cd4652d3ae8acca747809635a906a57b755f4
joachifm <joachifm@lavabit.com>
2013-08-03 21:44:35 +0200

    Sample: update haddocks

--- Sample.hs
+++ Sample.hs
@@ -13,18 +13,17 @@ import System.Random.MWC.CondensedTable
 import qualified Data.Vector.Unboxed as U
 
 ------------------------------------------------------------------------
-
--- | Compute a 'CondensedTableU' for drawing indices into a vector that
--- represents the \"population\".
+-- $drawtable
+--
+-- For weighted sampling, we compute 'CondensedTableU'S for the
+-- population indices.
 --
 -- That is, @drawTable n w@ returns a 'CondensedU' table for
--- @0..n-1@ where @n@ is the population size and @w@ are the probability
--- weights.
+--- @0..n-1@ where @n@ is the population size and @w@ are the weights.
 --
--- If no probability weights are supplied, all indices have equal
--- probability of being drawn.
---
--- Warning: the table will take up a lot of memory for large populations.
+-- If no weights are supplied, all indices are assigned equal weight.
+
+-- | Produce a 'CondensedTableU' for probability weighted sampling of population indices.
 drawTableP :: Int                     -- ^ Population size
           -> Maybe (U.Vector Double)  -- ^ Probability weights.
           -> CondensedTableU Int
@@ -36,9 +35,8 @@ drawTableP n = tableFromProbabilities . U.zip (U.enumFromN 0 n)
 
 -- | Perform a sample using a pre-computed 'CondensedTable'.
 --
--- This defintion is provided so that multiple samples may be
--- drawn from the same table within a single session, possibly
--- saving some work.
+-- @genFromTable tbl@ is assumed to return random indices into
+-- the supplied population vector @x@.
 sampleT :: U.Unbox e => U.Vector e
          -> Int
          -> CondensedTableU Int
@@ -51,11 +49,7 @@ sampleT x size tbl g = U.generateM size $ \_ -> do
 
 ------------------------------------------------------------------------
 
--- | Perform a weighted sample.
---
--- Note: unless a weighted sample is desired, use 'sampleU' instead.
--- The two are similar in performance, but 'sampleW' will not handle
--- samples from very large populations that well (see 'drawTable').
+-- | Perform a sample using probability weights.
 sampleP :: U.Unbox e => U.Vector e
         -> Int
         -> Maybe (U.Vector Double)
