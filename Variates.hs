c8632922ee4c4d3eeb4652f6671ae38f17eeabd7
joachifm <joachifm@lavabit.com>
2013-08-03 14:04:03 +0200

    Variates: add some wrappers for common variates
    Add runif, rnorm, and rbinom, attempting to emulate
    the functions found in GNU R.

--- Variates.hs
+++ Variates.hs
@@ -1,11 +1,44 @@
 module Variates
   ( variates
   , variatesU
+  , rbinom
+  , runif, runif'
+  , rnorm, rnorm'
   ) where
 
+import Data.Maybe (fromMaybe)
+
 import System.Random.MWC
+import System.Random.MWC.CondensedTable
+import System.Random.MWC.Distributions
+
 import qualified Data.Vector.Generic as G
 import qualified Data.Vector.Unboxed as U
+
+------------------------------------------------------------------------
+
+rbinom :: Int    -- ^ Number of obs.
+       -> Int    -- ^ Number of trials
+       -> Double -- ^ Probability of success
+       -> GenIO
+       -> IO (U.Vector Int)
+rbinom n siz prob = variates n $ genFromTable (tableBinomial siz prob)
+
+------------------------------------------------------------------------
+
+runif :: Int -> GenIO -> IO (U.Vector Double)
+runif n = runif' n Nothing
+
+runif' :: Int -> Maybe (Double, Double) -> GenIO -> IO (U.Vector Double)
+runif' n = variates n . uniformR . fromMaybe (0, 1)
+
+------------------------------------------------------------------------
+
+rnorm :: Int -> GenIO -> IO (U.Vector Double)
+rnorm n = rnorm' n Nothing
+
+rnorm' :: Int -> Maybe (Double, Double) -> GenIO -> IO (U.Vector Double)
+rnorm' n = variates n . uncurry normal . fromMaybe (0, 1)
 
 ------------------------------------------------------------------------
 
